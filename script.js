2822a6bc6ef3e149e63bdcb724dfedb2b71f1399
D_Zucho <danny.nico.dev@outlook.com>
2021-05-08 23:45:01 -0700

    add infinite scroll functionality

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = []
 
 // Unspace API
-const count = 10
+const count = 30
 const apiKey = 'BHKuvwmoFetwmnCbhzxmGSafYDd2D0xiwBGGnDLdMeo'
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// * Check if all Images Were Loaded
+function imageLoaded() {
+  imagesLoaded++
+  if (imagesLoaded === totalImages) {
+    ready = true
+    console.log('ready = ', ready)
+  }
+}
 
 //* Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -17,6 +29,9 @@ function setAttributes(element, attributes) {
 
 // * Create Elements for Links & Photos, Add to DOM
 function displayPhotos(photosArray) {
+  imagesLoaded = 0
+  totalImages = photosArray.length
+  console.log('total images: ', totalImages)
   photosArray.forEach((photo) => {
     //* Create elements for picture details
     const item = document.createElement('a')
@@ -26,14 +41,15 @@ function displayPhotos(photosArray) {
     })
 
     const image = document.createElement('img')
-    // image.setAttribute('src', photo.urls.regular)
-    // image.setAttribute('alt', photo.alt_description)
-    // image.setAttribute('title', photo.alt_description)
+
     setAttributes(image, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     })
+
+    // * Event listener, check when each is finished loading
+    image.addEventListener('load', imageLoaded)
 
     // * Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(image)
@@ -54,3 +70,14 @@ async function getPhotos() {
 
 // * On Load
 getPhotos()
+
+//* Event Listeners
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false
+    getPhotos()
+  }
+})
